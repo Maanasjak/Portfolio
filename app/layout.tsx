04f55bd3a60f9a21a536b68b03d5e36c9fd2ba6c
Maanasjak <156713748+Maanasjak@users.noreply.github.com>
2024-08-02 20:38:26 +0100

    "Added favicon and title metadata, and included Next's Head component in RootLayout"

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,16 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Maanas's Portfolio",
   description: "Best",
+  icons: {
+    icon: "/favicon.ico",
+  }
 };
 
 export default function RootLayout({
@@ -17,6 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/" />
+      </Head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
